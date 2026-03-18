90ffdf947cddeec8e42b29f82a9fba4f2fc39a36
joaoVitor1009 <joaovitorsg1804@gmail.com>
2026-03-18 16:05:47 -0300

    Category e Order

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -24,6 +25,7 @@ private static final long serialVersionUID = 1L;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -31,12 +33,14 @@ private static final long serialVersionUID = 1L;
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
-		super();
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		setOrderStatus(orderStatus);
+		this.client = client;
 	}
+
 
 	public Long getId() {
 		return id;
@@ -61,7 +65,16 @@ private static final long serialVersionUID = 1L;
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
 
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null)
+		this.orderStatus = orderStatus.getCode();
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
