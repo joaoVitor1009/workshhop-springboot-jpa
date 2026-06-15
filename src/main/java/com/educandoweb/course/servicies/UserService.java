0122e09715aaf0ff5217e31bb501d57debd873a6
joaoVitor1009 <joaovitorsg1804@gmail.com>
2026-06-15 13:25:16 -0300

    Update Funcionalidade

--- src/main/java/com/educandoweb/course/servicies/UserService.java
+++ src/main/java/com/educandoweb/course/servicies/UserService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
@@ -32,5 +33,17 @@ public class UserService  {
 	
 	public void delete(Long id) {
 		userRepository.deleteById(id);
+	}
+	@Transactional
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
